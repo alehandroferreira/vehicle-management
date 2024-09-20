d9aa475f8238b11318cabfd90c71ccdf8bbbc26f
Alehandro Ferreira <alehandro.ferreira@gmail.com>
2024-09-20 04:27:46 -0300

    feat: :construction: enabling the class validator

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,14 +6,20 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitando validação automática
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Configuração do Swagger
   const config = new DocumentBuilder()
-    .setTitle('Veichle Management')
+    .setTitle('Vehicle Management')
     .setDescription('API for managing cars and drivers')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Inicialização do servidor
   await app.listen(3000);
 }
 bootstrap();
